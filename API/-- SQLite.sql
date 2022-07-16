0886eb14dd899aa9093223068bda38fbb0104c3c
Clint Kingston <ckingston1@woolworths.com>
2022-07-17 09:37:49 +1000

    Angular client added

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Clint");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Lisa");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Hira");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Alex");
